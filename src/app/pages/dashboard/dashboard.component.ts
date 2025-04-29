380665ba68b848247acd35f555b192981865b71a
monika <monika@cashlinkglobal.com>
2025-04-29 18:08:43 +0530

    dashboard - profile - UI

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
 import { Product, ProductService } from '../service/product.service';
@@ -18,7 +18,7 @@ import { ChartModule } from 'primeng/chart';
     TableModule,
     RippleModule,
     ChartModule
-],
+  ],
   standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
@@ -29,14 +29,75 @@ export class DashboardComponent {
   chartData: any;
   chartOptions: any;
   subscription!: Subscription;
+  imageUrl: string | ArrayBuffer | null = null;
   items = [
     { label: 'Add New', icon: 'pi pi-fw pi-plus' },
     { label: 'Remove', icon: 'pi pi-fw pi-trash' }
   ];
-  
 
+  @ViewChild('fileInput') fileInput!: ElementRef;
   products!: Product[];
-
+  userInfo:any = {
+    "USERID": 24,
+    "USERNAME": "demomaker",
+    "USERPASSWORD": "EX0vrfvtYYZpSZzc+9a7hBX9H+o=",
+    "PROFILEID": 12,
+    "FIRSTNAME": "TEST",
+    "LASTNAME": "s",
+    "EMAILID": "Test123@gmail.com",
+    "USERSTATUS": "1",
+    "DESCRIPTION": "---",
+    "BRANCHCODE": "000",
+    "IPADDRESS": "N",
+    "RETRYCOUNT": 0,
+    "USERBLOCK": "0",
+    "EXPIRYDATE": "N",
+    "CREATIONDATE": 1720117800000,
+    "LOGINSTATUS": 0,
+    "FIRSTTIME": 0,
+    "PASSWORDEXPIRYDATE": "N",
+    "PASSWORDEXPIRYFLAG": 0,
+    "MODIFIEDDATE": null,
+    "INSTID": "TEST",
+    "CREATEDUSERID": "SU",
+    "FORGOTPASSWORDFLAG": "0",
+    "LASTLOGIN": "24-APR-25 16:25:25",
+    "USERTYPE": "IM",
+    "PSWREPEATCOUNT": "N",
+    "FRSTLOGINDATE": "16-APR-25",
+    "FRSTLOGIN": "L",
+    "FORCEPSWEXP": "N",
+    "SALT_KEY": "tz925I+/U9c=",
+    "CHANGPASS_DATE": 1744799304000,
+    "ADDED_BY": "superadmin1",
+    "ADDED_DATE": 1720157300000,
+    "AUTH_STATUS": "1",
+    "AUTH_BY": "superadmin2",
+    "AUTH_DATE": 1720157356000,
+    "REMARKS": "AUTHORIZED",
+    "UNBLOCKED_BY": null,
+    "UNBLOCKED_DATE": null,
+    "UNBLOCK_AUTHBY": null,
+    "UNBLOCK_AUTHDATE": null,
+    "PASSWDRESET_BY": "superadmin1",
+    "PASSWDRESET_DATE": 1744799293000,
+    "PASSWD_RESETAUTHBY": null,
+    "PASSWD_RESETDATE": null,
+    "DELETED_BY": null,
+    "DELETED_DATE": null,
+    "DELETED_FLAG": null,
+    "CBS_USERNAME": "adminmaker",
+    "MOBILENO": 9843645324,
+    "DOB": 1720117800000,
+    "COUNTRY": "India",
+    "CITY": "chennai",
+    "ADDRESS": "egmore",
+    "PROFILE_PIC": null,
+    "PROFILE_PIC_TYPE": null,
+    "UNBLOCK_TIME": null,
+    "BLOCKEDDATE": null,
+    "PASS_COUNT": null
+  }
   constructor(private productService: ProductService, public layoutService: LayoutService) {
     this.subscription = this.layoutService.configUpdate$.pipe(debounceTime(25)).subscribe(() => {
       this.initChart();
@@ -121,6 +182,27 @@ export class DashboardComponent {
         }
       }
     };
+  }
+
+  // Trigger file input click
+  triggerFileInputClick(): void {
+    // Trigger the click event on the file input element
+    this.fileInput.nativeElement.click();
+  }
+
+
+  onImageSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input?.files?.[0]) {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
   }
 
   ngOnDestroy() {
