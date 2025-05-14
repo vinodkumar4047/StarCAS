39d6c90ccb1c778c7088049131386f5fdf132c65
vinodkumar4047 <vinodkumar@cashlinkglobal.com>
2025-05-14 15:10:03 +0530

    table-saf,admin card

--- src/app/layout/component/table/table.component.ts
+++ src/app/layout/component/table/table.component.ts
@@ -3,6 +3,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SortEvent } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { Checkbox } from 'primeng/checkbox';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { TableModule } from 'primeng/table';
@@ -18,6 +19,7 @@ import { TooltipModule } from 'primeng/tooltip';
     InputIconModule,
     IconFieldModule,
     ButtonModule,
+    Checkbox
   ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
@@ -38,6 +40,7 @@ export class TableComponent {
   @Output() deleteEvent = new EventEmitter<any>();
   @Output() viewEvent = new EventEmitter<any>();
   @Output() event = new EventEmitter<any>();
+  @Output() rowSelected = new EventEmitter<any>();
   search: any = '';
   constructor() { }
 
@@ -75,10 +78,47 @@ export class TableComponent {
     this.event.emit({ data: data, event: event, type: type });
   }
 
-  customSort(event: SortEvent) {
-    console.log(event, 'event=====');
 
+  customSort(event: any) {
+    const { field, order } = event.multiSortMeta[0];
+    console.log(field, order, event);
+
+    // Get column metadata (type, etc.)
+    const column = this.cols.find((col: any) => col.field === field);
+    const columnType = column?.type;
+
+    // Sort by the column's field
+    this.tableData.sort((a: any, b: any) => {
+      const valA = a[field];
+      const valB = b[field];
+
+      // Handle string sorting
+      if (columnType === 'string') {
+        const comparison = valA.localeCompare(valB);
+        return order === 1 ? comparison : -comparison;
+      }
+
+      // Handle numeric sorting
+      if (columnType === 'number') {
+        return order === 1 ? valA - valB : valB - valA;
+      }
+
+      // Handle date sorting
+      if (columnType === 'date') {
+        const dateA = new Date(valA);
+        const dateB = new Date(valB);
+        return order === 1 ? dateA.getTime() - dateB.getTime() : dateB.getTime() - dateA.getTime();
+      }
+
+      // Fallback (non-sortable)
+      return 0;
+    });
   }
 
+
+  emitRowSelection(data: any, event: any) {
+    console.log({ data: data, event: event }, '{ data: data, event: event }');
+    this.rowSelected.emit({ data: data, event: event });
+  }
 
 }
--- src/app/pages/utility/admin-card/admin-card.component.ts
+++ src/app/pages/utility/admin-card/admin-card.component.ts
@@ -1,11 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabel } from 'primeng/floatlabel';
+import { InputText } from 'primeng/inputtext';
+import { DatePickerModule } from 'primeng/datepicker';
 
 @Component({
   selector: 'app-admin-card',
-  imports: [],
+  imports: [CommonModule, ButtonModule, FormsModule, ReactiveFormsModule, InputText, FloatLabel, DatePickerModule],
   templateUrl: './admin-card.component.html',
   styleUrl: './admin-card.component.scss'
 })
 export class AdminCardComponent {
+  AdminForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
+  ngOnInit() {
+    this.AdminForm = this.fb.group({
+      institutionName: ['TEST', Validators.required],
+      maximumCard: [null, Validators.required],
+      expDate: [null, Validators.required],
+    });
+    this.AdminForm.get('institutionName')?.disable();
+  }
 
+  onGenerate() {
+    if (this.AdminForm.valid) {
+      console.log('Form Data:', this.AdminForm.value);
+    } else {
+      console.log('Form is invalid', this.AdminForm);
+      this.AdminForm.markAllAsTouched();
+    }
+  }
+
+  reset() {
+    const name = this.AdminForm?.value?.institutionName
+    const formData = {
+      maximumCard: null,
+      expDate: null,
+      institutionName: null
+    };
+    this.AdminForm.reset(formData);
+    this.AdminForm.patchValue({
+      institutionName: 'TEST',
+    });
+
+  }
 }
