348d347c628198822f88f5abfa08324a8a006940
Kamaleshwar123 <kamaleshwar@cashlinkglobal.com>
2025-06-20 11:34:43 +0530

    transation

--- src/app/pages/monitorning/transaction-search/transaction-search.component.ts
+++ src/app/pages/monitorning/transaction-search/transaction-search.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component} from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MenuItem, MessageService } from 'primeng/api';
 import { SpeedDial, SpeedDialModule } from 'primeng/speeddial';
@@ -10,9 +10,11 @@ import { CommonModule } from '@angular/common';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { DatePicker } from 'primeng/datepicker';
 import { RestService } from '../../../layout/service/rest.service';
-import { take } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { HttpParams } from '@angular/common/http';
+
 @Component({
-changeDetection:ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-transaction-search',
   imports: [FormsModule, ToastModule, SpeedDialModule, ButtonModule, ReactiveFormsModule,
     InputTextModule, RadioButtonModule, CommonModule, FloatLabelModule, DatePicker],
@@ -21,7 +23,8 @@ changeDetection:ChangeDetectionStrategy.OnPush,
   providers: [MessageService]
 })
 export class TransactionSearchComponent {
-
+  TranSearchData: any
+  loading: boolean = true;
   date: Date | undefined;
   date2: Date | undefined;
   items: MenuItem[] = [];
@@ -65,13 +68,14 @@ export class TransactionSearchComponent {
       "COUNTRYDESC": "Armenia"
     }
   ];
-  constructor(private messageService: MessageService, private fb: FormBuilder, private restApi: RestService) { }
+  constructor(private messageService: MessageService, private cdr: ChangeDetectorRef, private fb: FormBuilder, private restApi: RestService) { }
 
   toggleRadioContent() {
     this.isSpeedDialOpen = !this.isSpeedDialOpen;
     this.showRadioOptions = this.isSpeedDialOpen;
   }
   ngOnInit() {
+
     this.searchForm = this.fb.group({
       RefNum: [''],
       Account: [''],
@@ -83,42 +87,42 @@ export class TransactionSearchComponent {
   }
 
   formatDate(date: Date): string {
-  const d = new Date(date);
-  const day = String(d.getDate()).padStart(2, '0');
-  const month = String(d.getMonth() + 1).padStart(2, '0');
-  const year = d.getFullYear();
-  return `${day}-${month}-${year}`;
-}
+    const d = new Date(date);
+    const day = String(d.getDate()).padStart(2, '0');
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const year = d.getFullYear();
+    return `${day}-${month}-${year}`;
+  }
 
   onSubmit() {
     if (this.searchForm.valid) {
       console.log('Form Data:', this.searchForm.value);
       // Process the form data here 
 
-   const formValue = this.searchForm.value;
-  const [fromDate, toDate] = formValue.FromToDate;
+      const formValue = this.searchForm.value;
+      const [fromDate, toDate] = formValue.FromToDate;
 
-  let payload: any = {
-    fromDate: this.formatDate(fromDate),
-    toDate: this.formatDate(toDate),
-    chnBased: '',
-    accountBased: '',
-    refnumBased: ''
-  };
+      let payload: any = {
+        fromDate: this.formatDate(fromDate),
+        toDate: this.formatDate(toDate),
+        chnBased: '',
+        accountBased: '',
+        refnumBased: ''
+      };
 
-  switch (formValue.Criteria) {
-    case '0':
-      payload.refnumBased = formValue.RefNum;
-      break;
-    case '1':
-      payload.accountBased = formValue.Account;
-      break;
-    case '2':
-      payload.chnBased = formValue.CHN;
-      break;
-  };
-  
-  console.log('Final Payload:', payload);
+      switch (formValue.Criteria) {
+        case '0':
+          payload.refnumBased = formValue.RefNum;
+          break;
+        case '1':
+          payload.accountBased = formValue.Account;
+          break;
+        case '2':
+          payload.chnBased = formValue.CHN;
+          break;
+      };
+
+      console.log('Final Payload:', payload);
 
       this.restApi.post(payload, '/monitoring/v1/transactionSearch').pipe(
         take(1),
@@ -137,4 +141,5 @@ export class TransactionSearchComponent {
       this.searchForm.markAllAsTouched();
     }
   }
+
 }
