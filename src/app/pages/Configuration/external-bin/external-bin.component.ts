a450964b4633b93c787ac977c0771fbeb380136c
vinodkumar4047 <vinovino891@gmail.com>
2025-06-24 10:15:47 +0530

    external bin in progress

--- src/app/pages/Configuration/external-bin/external-bin.component.ts
+++ src/app/pages/Configuration/external-bin/external-bin.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component} from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TableComponent } from '../../../layout/component/table/table.component';
 import { DialogModule } from 'primeng/dialog';
@@ -6,11 +6,14 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
+import { RestService } from '../../../layout/service/rest.service';
+import { take } from 'rxjs';
+import { DialogService } from '../../../layout/component/commonDialog.service';
 
 @Component({
-changeDetection:ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-external-bin',
-  imports: [TableComponent,DialogModule,ButtonModule,CommonModule,FormsModule,],
+  imports: [TableComponent, DialogModule, ButtonModule, CommonModule, FormsModule,],
   templateUrl: './external-bin.component.html',
   styleUrl: './external-bin.component.scss'
 })
@@ -22,63 +25,101 @@ export class ExternalBinComponent {
     'DESCRIPTION',
     'RECORDID',
   ];
-  
- rawData = [
-    { INSTID: 'TEST', BIN: '437551', DESCRIPTION: 'TEST', RECORDID: '4' },
-    { INSTID: 'TEST', BIN: '63980700', DESCRIPTION: 'TEST', RECORDID: '1' },
-    { INSTID: 'TEST', BIN: '611111', DESCRIPTION: 'TEST', RECORDID: '3' },
-    { INSTID: 'TEST', BIN: '605736', DESCRIPTION: 'TEST', RECORDID: '5' },
-    { INSTID: 'TEST', BIN: '451452', DESCRIPTION: 'TEST', RECORDID: '7' },
-    { INSTID: 'TEST', BIN: '466096', DESCRIPTION: 'TEST', RECORDID: '6' },
-    { INSTID: 'TEST', BIN: '607084', DESCRIPTION: 'TEST', RECORDID: '8' },
-  ];
-  
+
+  rawData = [];
+
   cols = [
     { field: 'INSTID', header: 'INST ID' },
-  { field: 'BIN', header: 'BIN' },
-  { field: 'DESCRIPTION', header: 'Description' },
-  { field: 'RECORDID', header: 'Record ID' },
-  { field: 'Action', header: 'Action' ,type:['view','edit','delete']},
+    { field: 'BIN', header: 'BIN' },
+    { field: 'DESCRIPTION', header: 'Description' },
+    { field: 'RECORDID', header: 'Record ID' },
+    { field: 'Action', header: 'Action', type: ['view', 'edit', 'delete'] },
   ];
 
-  editVisible:any;
-  Edit_data:any={
+  editVisible: any;
+  Edit_data: any = {
     INSTID: '',
     BRANCHID: '',
     MAPCODE: '',
     BRANCHNAME: ''
   };
-tpCheck!:boolean;
+  tpCheck!: boolean;
   buttonsList: any = [
     { label: 'Authorize Delete External BIN', icon: 'pi pi-user-minus', type: 'deleteAuth', variant: 'outlined', severity: "danger" },
     { label: 'Authorize External BIN', icon: 'pi pi-verified', type: 'auth', variant: 'outlined', severity: "info" }
   ]
   userRole: any = localStorage.getItem('userRole');
   userType: any = localStorage.getItem('userType');
-constructor(private router: Router) { };
+  loading: any;
+  constructor(private router: Router, private restApi: RestService, private cdr: ChangeDetectorRef,private dialogService:DialogService) { };
 
-    ngOnInit(){
-    this.cols = this.userType === 'maker'
-  ? this.cols
-  : this.cols.filter(col => col.field !== 'Action');
+  ngOnInit() {
+    this.cols = this.userType === 'M'
+      ? this.cols
+      : this.cols.filter(col => col.field !== 'Action');
+       this.getList();
   }
 
-  delateData(){
+  getList() {
+    this.restApi.get('/configuration/external-bin/list').pipe(
+      take(1)
+    ).subscribe({
+      next: (res) => {
+        if (res) {
+          this.rawData = res;
+          console.log(res,'res----');
+          
+        } else {
+          console.warn('No data received or request failed.');
+        }
+        this.loading = false;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error('Subscription error:', err);
+        this.loading = false;
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+  delateData() {
     this.delete_visible = false
   }
 
-  edit(data:any,type:any){
-    this.Edit_data = {...data?.data};
+  edit(data: any, type: any) {
+    this.Edit_data = { ...data?.data };
     console.log(data);
-    this.tpCheck = type == 'View' ? true:false;
+    this.tpCheck = type == 'View' ? true : false;
     this.editVisible = true;
   }
 
-  authFunction(event:any){
-     this.router.navigate(['/pages/auth-ext-bin'], { state: { type: event.type } });
+  authFunction(event: any) {
+    this.router.navigate(['/pages/auth-ext-bin'], { state: { type: event.type } });
   }
 
-  addOrEdit(type:any,data:any){
-    this.router.navigate(['/pages/add_externalBin'], { state:{ data:data?.data , type:type} });
+  addOrEdit(type: any, data: any) {
+    this.router.navigate(['/pages/add_externalBin'], { state: { data: data?.data, type: type } });
+  }
+
+  editExternalBin(){
+      let payload = {}
+      //   "instId": this.extBinForm.value?.Instution,
+      //   "bin": this.extBinForm.value?.BIN,
+      //   "description": this.extBinForm.value?.Description
+      // }
+      this.restApi.post(payload, '/configuration/external-bin/add').subscribe({
+        next: (res) => {
+         
+            console.log('User added successfully:', res);
+                this.editVisible = false
+            this.dialogService.show('Success', res?.message, 'success',3000);
+        },
+        error: (err) => {
+          console.error('Error adding profile:', err)
+          this.dialogService.show('Oops!', err.message, 'error', 3000); // ✅ Error dialog
+        }
+      });
+
   }
 }
